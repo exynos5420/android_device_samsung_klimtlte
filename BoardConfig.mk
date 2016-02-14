efe343191b3d3c4abdb8f7d649c9569b0c46ca2c
Sean Hoyt <seanhoyt963@gmail.com>
2016-02-14 06:14:07 -0600

    Add hardware_samsung cmhw's
    We support Glove mode, touchscreen hovering, adaptive backlight

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,6 +100,7 @@ TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Hardware
 BOARD_HARDWARE_CLASS += device/samsung/klimtlte/cmhw
+BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
 
 # Init
 TARGET_NR_SVC_SUPP_GIDS := 20
