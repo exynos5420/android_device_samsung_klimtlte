4d3377d754339456909e1caf0448dc79e8040abd
deadman96385 <seanhoyt963@gmail.com>
2016-03-21 15:22:57 -0500

    Import cbd from vendor

--- device.mk
+++ device.mk
@@ -227,7 +227,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-swap=false
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.carrier=unknown 
+    ro.carrier=unknown
+
+# cpboot-daemon for modem
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ril/sbin/cbd:root/sbin/cbd
 
 # call dalvik heap config
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
