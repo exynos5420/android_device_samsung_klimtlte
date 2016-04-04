ec6134dda8f6dc9921df4913961a54c14087262f
deadman96385 <seanhoyt963@gmail.com>
2016-04-04 06:20:38 -0500

    Fix power script location
    Change-Id: I5b78737cefb8cc5e89a4d469fe2ba75abded5527

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -28,7 +28,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.universal5420.power.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= BOOT
-LOCAL_SRC_FILES		:= init.universal5420.power.rc
+LOCAL_SRC_FILES		:= etc/init.universal5420.power.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
