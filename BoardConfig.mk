98dcd8c5fde69f8a0b14cf8db89c57bb23bef3c2
deadman96385 <seanhoyt963@gmail.com>
2016-04-04 06:20:06 -0500

    Switch to deathly defconfig
    Change-Id: I6020283b887393f0e240dccb5916509b812e46be

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,7 +63,7 @@ COMMON_GLOBAL_CFLAGS += -DFORCE_SCREENSHOT_CPU_PATH
 # Kernel
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_CONFIG := cyanogenmod_klimtlte_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_deathly_klimtlte_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/klimtwifi
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.8
 
