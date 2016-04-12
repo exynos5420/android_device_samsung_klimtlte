5ff00a6751829a9610df3752c547862606155b70
deadman96385 <seanhoyt963@gmail.com>
2016-04-12 01:35:09 -0500

    Enable modem needs video call field
    Needed for:
    http://review.cyanogenmod.org/#/c/139209/

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,6 +40,7 @@ BOARD_PROVIDES_LIBRIL := true
 BOARD_MODEM_TYPE := xmm7260
 # RIL.java overwrite
 BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
+BOARD_MODEM_NEEDS_VIDEO_CALL_FIELD := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
