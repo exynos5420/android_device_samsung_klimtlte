c303e9aaa10c1c7b78bd7dff3871a1758e9ff1f5
deadman96385 <seanhoyt963@gmail.com>
2016-06-17 15:04:36 -0500

    Remove /etc folder

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= fstab.universal5420
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/fstab.universal5420
+LOCAL_SRC_FILES		:= fstab.universal5420
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -12,7 +12,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.universal5420.usb.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.universal5420.usb.rc
+LOCAL_SRC_FILES		:= init.universal5420.usb.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -20,7 +20,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.universal5420.wifi.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.universal5420.wifi.rc
+LOCAL_SRC_FILES		:= init.universal5420.wifi.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -28,7 +28,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.universal5420.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.universal5420.rc
+LOCAL_SRC_FILES		:= init.universal5420.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -36,7 +36,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.samsung.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.samsung.rc
+LOCAL_SRC_FILES		:= init.samsung.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -44,7 +44,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.universal5420.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/ueventd.universal5420.rc
+LOCAL_SRC_FILES		:= ueventd.universal5420.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -52,16 +52,16 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.recovery.universal5420.usb.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.recovery.universal5420.usb.rc
+LOCAL_SRC_FILES		:= init.recovery.universal5420.usb.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := init.baseband.rc
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.baseband.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_MODULE        := init.baseband.rc
+LOCAL_MODULE_TAGS   := optional eng
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_SRC_FILES     := init.baseband.rc
+LOCAL_MODULE_PATH   := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 
