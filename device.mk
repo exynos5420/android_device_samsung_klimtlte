58de15f4aa352ca784571b169e4e3b189e3357f2
Sean Hoyt <seanhoyt963@gmail.com>
2016-08-22 22:57:21 -0700

    Require vendor
    This has annoyed me one to many times

--- device.mk
+++ device.mk
@@ -247,7 +247,7 @@ $(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
 $(call inherit-product, hardware/samsung_slsi-cm/exynos5420/exynos5420.mk)
 
 # call the proprietary setup
-$(call inherit-product-if-exists, vendor/samsung/klimtlte/klimtlte-vendor.mk)
+$(call inherit-product, vendor/samsung/klimtlte/klimtlte-vendor.mk)
 
 # call the proprietary setup
-$(call inherit-product-if-exists, vendor/samsung/tabs-common/tabs-common-vendor.mk)
+$(call inherit-product, vendor/samsung/tabs-common/tabs-common-vendor.mk)
