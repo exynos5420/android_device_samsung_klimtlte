320aff22fec78c60febd1d12e336f41570611833
Paul Keith <javelinanddart@gmail.com>
2016-09-19 16:07:15 -0700

    consumerir: Cleanup HAL
    * Unify HAL
    * Take some styling changes from JF, thanks to @zwliew
    
    Change-Id: Ia24ddc708801540b1cc34db787975761f55b8dab
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- hal/consumerir/consumerir.c
+++ hal/consumerir/consumerir.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2013 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,84 +15,91 @@
  */
 #define LOG_TAG "ConsumerIrHal"
 
-#include <stdlib.h>
+#include <errno.h>
 #include <malloc.h>
 #include <stdbool.h>
-#include <errno.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <pthread.h>
 #include <cutils/log.h>
 #include <hardware/hardware.h>
 #include <hardware/consumerir.h>
 
+#define UNUSED __attribute__((unused))
+
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
 static const consumerir_freq_range_t consumerir_freqs[] = {
+#ifdef USE_ONE_FREQ_RANGE
+    {.min = 16000, .max = 60000},
+#else
     {.min = 30000, .max = 30000},
     {.min = 33000, .max = 33000},
     {.min = 36000, .max = 36000},
     {.min = 38000, .max = 38000},
     {.min = 40000, .max = 40000},
     {.min = 56000, .max = 56000},
+#endif
 };
 
-static bool
-try_append_number(char *buffer, int *len, int size, int number)
+static bool try_append_number(char *buffer, int *len, int size, int number)
 {
     int stored;
 
     stored = snprintf(&buffer[*len], size - *len, "%d,", number);
 
-    if (stored < 0 || stored >= size - *len) {
+    if (stored < 0 || stored >= (size - *len))
         return false;
-    }
 
     *len += stored;
     return true;
 }
 
-static bool
-grow_buffer(char **buffer, int *size)
+static bool grow_buffer(char **buffer, int *size)
 {
     char *new_buffer;
 
     *size *= 2;
-    if ((new_buffer = realloc(*buffer, *size)) == NULL) {
+    new_buffer = realloc(*buffer, *size);
+    if (new_buffer == NULL)
         return false;
-    }
+
     *buffer = new_buffer;
     return true;
 }
 
-static bool
-append_number(char **buffer, int *len, int *size, int number)
+static bool append_number(char **buffer, int *len, int *size, int number)
 {
-    if (! try_append_number(*buffer, len, *size, number)) {
-        if (! grow_buffer(buffer, size)) return false;
-        return try_append_number(*buffer, len, *size, number);
-    } else {
+    if (try_append_number(*buffer, len, *size, number))
         return true;
-    }
+
+    if (!grow_buffer(buffer, size))
+        return false;
+
+    return try_append_number(*buffer, len, *size, number);
 }
 
+
+pthread_mutex_t g_mtx;
 int fd = 0;
-static int consumerir_transmit(struct consumerir_device *dev,
-   int carrier_freq, int pattern[], int pattern_len)
+static int consumerir_transmit(UNUSED struct consumerir_device *dev,
+   int carrier_freq, const int pattern[], int pattern_len)
 {
+    pthread_mutex_lock(&g_mtx);
     int buffer_len = 0;
     int buffer_size = 128;
     int i;
     char *buffer;
 
-    if ((buffer = malloc(buffer_size)) == NULL) {
+    buffer = malloc(buffer_size);
+    if (buffer == NULL)
         return -ENOMEM;
-    }
 
     /* write the header */
-    if (! append_number(&buffer, &buffer_len, &buffer_size, carrier_freq)) {
+    if (!append_number(&buffer, &buffer_len, &buffer_size, carrier_freq))
         goto error;
-    }
 
     /* calculate factor of conversion from microseconds to pulses */
     float factor = 1000000 / carrier_freq;
@@ -100,7 +107,8 @@ static int consumerir_transmit(struct consumerir_device *dev,
     /* write out the timing pattern */
     for (i = 0; i < pattern_len; i++)
     {
-        if (! append_number(&buffer, &buffer_len, &buffer_size, (int) (pattern[i]/factor))) {
+        if (!append_number(&buffer, &buffer_len, &buffer_size,
+                (int) (pattern[i] / factor))) {
             goto error;
         }
     }
@@ -110,6 +118,7 @@ static int consumerir_transmit(struct consumerir_device *dev,
 
     free(buffer);
 
+    pthread_mutex_unlock(&g_mtx);
     return 0;
 
 error:
@@ -117,12 +126,12 @@ error:
     return -ENOMEM;
 }
 
-static int consumerir_get_num_carrier_freqs(struct consumerir_device *dev)
+static int consumerir_get_num_carrier_freqs(UNUSED struct consumerir_device *dev)
 {
     return ARRAY_SIZE(consumerir_freqs);
 }
 
-static int consumerir_get_carrier_freqs(struct consumerir_device *dev,
+static int consumerir_get_carrier_freqs(UNUSED struct consumerir_device *dev,
     size_t len, consumerir_freq_range_t *ranges)
 {
     size_t to_copy = ARRAY_SIZE(consumerir_freqs);
@@ -136,6 +145,7 @@ static int consumerir_close(hw_device_t *dev)
 {
     free(dev);
     close(fd);
+    pthread_mutex_destroy(&g_mtx);
     return 0;
 }
 
@@ -145,6 +155,8 @@ static int consumerir_close(hw_device_t *dev)
 static int consumerir_open(const hw_module_t* module, const char* name,
         hw_device_t** device)
 {
+    pthread_mutex_init(&g_mtx, NULL);
+
     if (strcmp(name, CONSUMERIR_TRANSMITTER) != 0) {
         return -EINVAL;
     }
