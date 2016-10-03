2edbe572e7bd83e16070242e41a23a8b968d155d
Christopher N. Hesse <raymanfx@gmail.com>
2016-10-03 15:00:33 +0200

    klimtlte: Enable freeform windows
    Change-Id: I039afd04d17f87f2b3070d915c8243210f075df4

--- device.mk
+++ device.mk
@@ -169,6 +169,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+	frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
 # Power
