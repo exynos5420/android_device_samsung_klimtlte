095688f77c406c500292dae1169cadfa8816f3f6
Sean Hoyt <seanhoyt963@gmail.com>
2016-10-12 05:02:06 -0500

    Switch the cmhw include orders
    Needed to include our custom glove mode which enables it for the touchkeys
    
    Change-Id: Id6063ef14d2ac4aca836447a83c5451fc2873e55

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,8 +105,8 @@ BOARD_USES_HWC_SERVICES := true
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Hardware
-BOARD_HARDWARE_CLASS += device/samsung/klimtlte/cmhw
 BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
+BOARD_HARDWARE_CLASS += device/samsung/klimtlte/cmhw
 
 # Keymaster
 BOARD_USES_TRUST_KEYMASTER := true
