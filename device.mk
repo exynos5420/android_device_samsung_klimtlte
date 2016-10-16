e996a860dec49a2c823f8b9373958190fb7ae53a
Sean Hoyt <seanhoyt963@gmail.com>
2016-10-16 16:05:51 -0500

    Move audio_effects to the proper folder
    Change-Id: I221213714ad2ec6ecdb8f9be33c2ce424e54a3eb

--- device.mk
+++ device.mk
@@ -35,7 +35,7 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf \
+    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
 
 # Boot animation
