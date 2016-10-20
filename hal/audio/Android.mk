980d410ac7ef5a04b4e349bbbd8f96f640fc102d
deadman96385 <seanhoyt963@gmail.com>
2016-10-20 18:33:47 -0500

    Use the newer i9500 audio hal
    Change-Id: I39d0918c2cce524f8fbd8b12f2a0f0302673d927

--- hal/audio/Android.mk
+++ hal/audio/Android.mk
@@ -18,19 +18,25 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := audio.primary.$(TARGET_BOOTLOADER_BOARD_NAME)
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := audio_hw.c ril_interface.c
 
 LOCAL_C_INCLUDES += \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
 	external/tinyalsa/include \
 	$(call include-path-for, audio-effects) \
 	$(call include-path-for, audio-utils) \
 	$(call include-path-for, audio-route)
 
+LOCAL_ADDITIONAL_DEPENDENCIES += \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
 LOCAL_SHARED_LIBRARIES := liblog libcutils libtinyalsa libaudioutils libdl \
 	libaudience_voicefx libaudioroute
+
+LOCAL_CFLAGS := -Wno-unused-parameter
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -41,21 +47,13 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libaudience_voicefx
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES:= eS325VoiceProcessing.cpp
+LOCAL_SRC_FILES := eS325VoiceProcessing.cpp
 
 LOCAL_C_INCLUDES += \
 	$(call include-path-for, audio-effects)
 
 LOCAL_SHARED_LIBRARIES := liblog libutils
 
+LOCAL_CFLAGS := -Wno-unused-parameter
+
 include $(BUILD_SHARED_LIBRARY)
-
-
-# Mixer configurations
-include $(CLEAR_VARS)
-LOCAL_MODULE := mixer_paths.xml
-LOCAL_MODULE_TAGS := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES := mixer_paths.xml
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
-include $(BUILD_PREBUILT)
