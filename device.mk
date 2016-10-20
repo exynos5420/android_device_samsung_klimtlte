1d11d2ec4d09b3f043797da9cdebdd6646b94b5e
deadman96385 <seanhoyt963@gmail.com>
2016-10-20 18:33:47 -0500

    Use the newer i9500 audio hal
    Change-Id: I39d0918c2cce524f8fbd8b12f2a0f0302673d927

--- device.mk
+++ device.mk
@@ -31,12 +31,12 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.usb.default \
     audio.r_submix.default \
-    mixer_paths.xml \
     tinymix
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf \
-    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
+    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Boot animation
 TARGET_BOOTANIMATION_HALF_RES := true
