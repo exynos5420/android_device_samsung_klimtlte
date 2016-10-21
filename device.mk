ede48cef48edce2f3a664107ffc73719185eabd8
Sean Hoyt <seanhoyt963@gmail.com>
2016-10-20 20:25:45 -0500

    Add prebuilt DPI sizes
    Change-Id: I355dc1d95855353750feb4d7951431602ef72d79

--- device.mk
+++ device.mk
@@ -24,6 +24,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_PREBUILT_DPI := xhdpi hdpi
 
 # Audio
 PRODUCT_PACKAGES += \
