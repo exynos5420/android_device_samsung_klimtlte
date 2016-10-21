769a951a2a7fac3450cdb9da241d8681125fc300
Sean Hoyt <seanhoyt963@gmail.com>
2016-10-20 20:25:45 -0500

    Add prebuilt DPI sizes
    Change-Id: I355dc1d95855353750feb4d7951431602ef72d79

--- device.mk
+++ device.mk
@@ -22,8 +22,9 @@ PRODUCT_CHARACTERISTICS := tablet
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_CONFIG := normal large xlarge
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_PREBUILT_DPI := xhdpi hdpi
 
 # Audio
 PRODUCT_PACKAGES += \
