e73789e92adf719981a5f555b71bec7232ba3985
ShevT <shev_t@mail.ru>
2016-10-24 22:41:57 +0300

    klimtlte: Add exynos MPEG2 and WMC Decoders
    Change-Id: I4672e7900156430f7df2e50f0add87ec6794f1d6

--- device.mk
+++ device.mk
@@ -139,7 +139,9 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
-    libcsc
+    libcsc \
+    libOMX.Exynos.WMV.Decoder \
+    libOMX.Exynos.MPEG2.Decoder
 
 # Permissions
 PRODUCT_COPY_FILES += \
