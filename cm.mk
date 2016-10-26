5780c7bf47abc57668652c0689c3fd45a3f0b084
Sean Hoyt <seanhoyt963@gmail.com>
2016-10-26 15:20:43 -0500

    Use Exynos5420-common
    Change-Id: I829f59cbd2b77736e7181e52d7742286103d7748

--- cm.mk
+++ cm.mk
@@ -1,4 +1,9 @@
-$(call inherit-product, device/samsung/klimtlte/full_klimtlte.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
+
+# Inherit from klimtlte device
+$(call inherit-product, device/samsung/klimtlte/device.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
