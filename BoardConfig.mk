1f7430412d479a5d6897dd266c3583d4180cc1dd
Sean Hoyt <seanhoyt963@gmail.com>
2016-10-26 20:14:45 -0500

    Enable extended filesystem support
    Change-Id: I03e889d58b8f9d942dffd29c6967aa43456ab1e4
    Credit: @Shevt

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,6 +108,10 @@ BOARD_HARDWARE_CLASS += device/samsung/klimtlte/cmhw
 # Keymaster
 BOARD_USES_TRUST_KEYMASTER := true
 
+# Extended filesystem support
+TARGET_KERNEL_HAVE_EXFAT := true
+TARGET_KERNEL_HAVE_NTFS := true
+
 # Media
 COMMON_GLOBAL_CFLAGS += -DUSE_NATIVE_SEC_NV12TILED # use format from fw/native
 COMMON_GLOBAL_CFLAGS += -DWIDEVINE_PLUGIN_PRE_NOTIFY_ERROR
