a36da37f6d1e96b99f392afe2178d89ea4428d0c
Sean Hoyt <seanhoyt963@gmail.com>
2016-12-31 04:05:39 -0600

    Enable DT2W support
    Change-Id: I2d788deef164381c68659459bb96bef55ddcdd92

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,9 @@ BOARD_MODEM_NEEDS_VIDEO_CALL_FIELD := true
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 
+# DT2W
+TARGET_DT2W_PATH := /sys/class/input/input1/wake_gesture
+
 # Bootloader
 TARGET_OTA_ASSERT_DEVICE := klimtlte
 
