9abea78fa06d59f74a46c7d5961f7d9dcb01f4f8
Thomas Patry <thompatry@gmail.com>
2017-01-07 17:29:33 -0600

    Revert "Rebrand to LineageOS"
    Lineage is not rebranding the CM-13.0 branch.
    
    This reverts commit ceaeea33ba226e5228cc58df75e49f7784ad94a5.
    
    Change-Id: Iabc3a687c190834015312bd690245ecf32a80e27

--- cm.mk
+++ cm.mk
@@ -7,13 +7,13 @@ $(call inherit-product, device/samsung/klimtlte/device.mk)
 
 PRODUCT_BRAND := samsung
 
-# Inherit some common LineageOS stuff.
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-# Inherit more LineageOS stuff.
+# Inherit more cyanogenmod stuff.
 $(call inherit-product, vendor/cm/config/telephony.mk)
 
-PRODUCT_NAME := lineage_klimtlte
+PRODUCT_NAME := cm_klimtlte
 PRODUCT_DEVICE := klimtlte
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo lineage_klimtlte-userdebug
+add_lunch_combo cm_klimtlte-userdebug
