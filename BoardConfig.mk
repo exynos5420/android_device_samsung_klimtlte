194ba025a4ccf5de3f129bc56790ddb64a5764d4
Sean Hoyt <seanhoyt963@gmail.com>
2017-01-08 22:30:22 -0600

    Do not use local path for selinux
    Change-Id: Ib54422b0f0e853effd4948f271aeb8fc85fa0ede

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,7 +53,7 @@ BOARD_FLASH_BLOCK_SIZE := 4096
 
 # SELinux
 BOARD_SEPOLICY_DIRS += device/samsung/exynos5420-common/sepolicy
-BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += device/samsung/klimtlte/sepolicy
 
 # Inherit from exynos5420-common
 include device/samsung/exynos5420-common/BoardConfigCommon.mk
