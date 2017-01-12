55148439120a53f7be308a466c6fb157be40bd15
Thomas Patry <thompatry@gmail.com>
2017-01-11 22:20:07 -0600

    Uncommonize fstab and move it back to the devices
    Change-Id: I66e579109718887eb419bd1942b11c82311e7f4a

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -1,6 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE            := fstab.universal5420
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := fstab.universal5420
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.baseband.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
