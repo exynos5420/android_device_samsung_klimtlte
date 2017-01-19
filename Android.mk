5261b6399a5b004f6eff94a0e20a6b73e1798a30
Sean Hoyt <seanhoyt963@gmail.com>
2017-01-19 12:11:19 -0500

    Make this a common repo for the other klimtlte variants
    This will allow T705W,T805M,T805Y to have their own minimal device trees
    
    Also convert it to lineage
    
    Change-Id: I7d5a14b65c172a57c789fb1bec572023b6f30078

--- Android.mk
+++ Android.mk
@@ -16,8 +16,8 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),klimtlte)
+ifneq ($(filter klimtlte klimtltedo klimtlteub klimtltecan,$(TARGET_DEVICE)),)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 endif
