1394af8c210421ba793e7017aa9efe4e6422d721
Stricted <info@stricted.net>
2018-09-22 20:54:11 +0200

    klimtlte: initial device tree
    Change-Id: Ie96634d5524f4b0a7673d1f8fb0e0f14c7a69949

---
+++ README.md
@@ -0,0 +1,22 @@
+Copyright 2020 - The LineageOS Project
+
+Device configuration for Samsung Galaxy Tab S 8.4 (SM-T705).
+========================================
+
+Basic   | Specification List
+-------:|:-------------------------
+CPU     | Quad-core 1.9 GHz + Quad-core 1.3 GHz
+Chipset | Samsung Exynos 5420
+GPU     | ARM Mali-T628 MP6
+Memory  | 3 GB
+Shipped Android Version | 4.4.2
+Storage | 16/32 GB
+MicroSD | Up to 256 GB
+Battery | Li-Ion 4900 mAh
+Dimensions | 218,8 mm x 125.6 mm x 6,6 mm
+Display | 2560 x 1600 pixel, 8.4"
+Rear Camera  | 8.0 MP AF
+Front Camera | 2.1 MP
+Release Date | July 2014
+
+![Galaxy Tab S 8.4](https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-tab-s-84-1.jpg "Galaxy Tab S 8.4")
---
+++ lineage_klimtlte.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2014-2016 The CyanogenMod Project
+# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from klimtlte device
+$(call inherit-product, device/samsung/klimtlte/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := klimtlte
+PRODUCT_NAME := lineage_klimtlte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-T705
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=klimtltexx \
+    PRIVATE_BUILD_DESC="klimtltexx-user 6.0.1 MMB29K T705XXU1CQB1 release-keys"
+
+BUILD_FINGERPRINT := samsung/klimtltexx/klimtlte:6.0.1/MMB29K/T705XXU1CQB1:user/release-keys
