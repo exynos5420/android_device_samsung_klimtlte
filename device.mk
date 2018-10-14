2ebef0320ad1b1c9454e820311fa42ec991ec97d
Stricted <info@stricted.net>
2018-10-15 00:11:48 +0200

    klimtlte: add device specific overlays
    Change-Id: I7df6c0e1aa8f932d6e69cd6f5e140e1683d746d7

--- device.mk
+++ device.mk
@@ -16,6 +16,8 @@
 
 LOCAL_PATH := device/samsung/klimtlte
 
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
