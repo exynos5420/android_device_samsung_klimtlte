261f6325319f9d2c66e169aa290482c3123e1d14
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 17:20:32 +0430

    klimtlte: Drop module eng tags
    Change-Id: I2e84d598b2412e617e5575bfdefd804612f76889

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -4,7 +4,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.baseband.rc
-LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/init.baseband.rc
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
@@ -12,7 +12,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.target.rc
-LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/init.target.rc
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
