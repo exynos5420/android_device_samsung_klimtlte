81ca083d37ca5f1c9b89b06396d15d5631cd456f
Jan Altensen <info@stricted.net>
2019-09-10 11:52:20 +0200

    klimtlte: gpsd: replace SSLv3_client_method with SSLv23_method
    Change-Id: I7aa2dc778d01bc91f2e4572143ccb37ec5d17a75

--- extract-files.sh
+++ extract-files.sh
@@ -74,4 +74,7 @@ patchelf --replace-needed libprotobuf-cpp-full.so libprotobuf-cpp-haxx.so $BLOB_
 sed -i "s|system/etc|vendor/etc|g" $BLOB_ROOT/vendor/lib/libfloatingfeature.so
 sed -i "s|system/etc|vendor/etc|g" $BLOB_ROOT/vendor/lib/libsec-ril.so
 
+# replace SSLv3_client_method with SSLv23_method
+sed -i "s/SSLv3_client_method/SSLv23_method\x00\x00\x00\x00\x00\x00/" $BLOB_ROOT/vendor/bin/gpsd
+
 "${MY_DIR}/setup-makefiles.sh"
