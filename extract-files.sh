66c9cb63b28293622c901901d84daab49c7ef305
Michael Bestas <mkbestas@lineageos.org>
2023-02-22 21:02:56 +0000

    klimtlte: Switch to standalone extract utils
    * Align with templates while we are at it
    
    Change-Id: Id18dcad7d1e08c09185af6b49b3ad875ddd86ff9

--- extract-files.sh
+++ extract-files.sh
@@ -1,72 +1,36 @@
 #!/bin/bash
 #
-# Copyright (C) 2018-2021 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/libsec-ril.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-haxx.so" "${2}"
+            (perl -pi -e "s/\/system\/bin\/gpsd/\/vendor\/bin\/gpsd/g" "${2}")
+            ;;
+        vendor/lib/libfloatingfeature.so)
+            sed -i "s|system/etc|vendor/etc|g" "${2}"
+            ;;
+        vendor/lib/libsec-ril.so)
+            sed -i "s/SSLv3_client_method/SSLv23_method\x00\x00\x00\x00\x00\x00/" "${2}"
+            ;;
+    esac
+}
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
 set -e
 
-DEVICE=klimtlte
-VENDOR=samsung
+export DEVICE=klimtlte
+export DEVICE_COMMON=klimtlte
+export VENDOR=samsung
 
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
-
-ANDROID_ROOT="${MY_DIR}/../../.."
-
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
-    exit 1
-fi
-source "${HELPER}"
-
-# Default to sanitizing the vendor folder before extraction
-CLEAN_VENDOR=true
-
-while [ "${#}" -gt 0 ]; do
-    case "${1}" in
-        -n | --no-cleanup )
-                CLEAN_VENDOR=false
-                ;;
-        -k | --kang )
-                KANG="--kang"
-                ;;
-        -s | --section )
-                SECTION="${2}"; shift
-                CLEAN_VENDOR=false
-                ;;
-        * )
-                SRC="${1}"
-                ;;
-    esac
-    shift
-done
-
-if [ -z "${SRC}" ]; then
-    SRC="adb"
-fi
-
-# Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
-
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-
-# Fix proprietary blobs
-BLOB_ROOT="$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
-
-# Replace libs to fix missing symbols
-"${PATCHELF}" --replace-needed libprotobuf-cpp-full.so libprotobuf-cpp-full-v23.so $BLOB_ROOT/vendor/lib/libsec-ril.so
-"${PATCHELF}" --replace-needed libcutils.so libcutils-v29.so $BLOB_ROOT/vendor/lib/libsec-ril.so
-
-# Vendor separation
-(perl -pi -e "s/\/system\/bin\/gpsd/\/vendor\/bin\/gpsd/g" $BLOB_ROOT/vendor/lib/libsec-ril.so)
-sed -i "s|system/etc|vendor/etc|g" $BLOB_ROOT/vendor/lib/libfloatingfeature.so
-sed -i "s|system/etc|vendor/etc|g" $BLOB_ROOT/vendor/lib/libsec-ril.so
-
-# replace SSLv3_client_method with SSLv23_method
-sed -i "s/SSLv3_client_method/SSLv23_method\x00\x00\x00\x00\x00\x00/" $BLOB_ROOT/vendor/bin/gpsd
-
-"${MY_DIR}/setup-makefiles.sh"
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
